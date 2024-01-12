ba31220d449f459a991c8478dc6c694501170758
gmflau <gmflau@gmail.com>
2024-01-11 17:36:04 -0800

    Removed unnecessary input

--- get_msstats.sh
+++ get_msstats.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 service_account"
-    exit 1
-fi
-
 projects=$(gcloud projects list | awk 'NR>1 {print $1}')
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
